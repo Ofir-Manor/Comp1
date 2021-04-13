91308b61e408c45345c40caf7842a412423bd69e
Ofir-Manor <ofir.manor@technion.ac.il>
2021-04-13 18:32:50 +0300

    Part b complete pre tests

--- HW1/part_b.cpp
+++ HW1/part_b.cpp
@@ -89,7 +89,7 @@ int main()
                 }
                 else
                 {
-                    printf("Error: ")
+                    printf("Error: %s\n", token_name(token));
                 }
         }
 
